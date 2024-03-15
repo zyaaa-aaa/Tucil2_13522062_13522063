8d6ae51f6f89d53dce856591dda6ff5d3e65a4cb
shazyaaa <shazyaaudrea@gmail.com>
2024-03-15 18:29:27 +0700

    new main file

---
+++ src/main.py
@@ -0,0 +1,50 @@
+from Quadratic_Brute import quadratic_bezier, plot_curve
+from Quadratic_DnC import bezier_curve, plot_bezier_curve, Point
+import time
+
+print("=======================================")
+print("              Bezier Curve             ")
+print("=======================================")
+print("1. Quadratic")
+print("2. Exit")
+print("=======================================")
+choice = int(input("Choice: "))
+print("=======================================")
+stop_program = False
+while(1 <= choice <= 2 and stop_program == False):
+    if(choice == 1):
+        x1, y1 = input("Masukkan titik kontrol 1: ").split()
+        x2, y2 = input("Masukkan titik kontrol 2 (titik kontrol tengah): ").split()
+        x3, y3 = input("Masukkan titik kontrol 3: ").split()
+        iterations = int(input("Masukkan jumlah iterasi: "))
+
+        ctrl1 = Point(float(x1), float(y1))
+        ctrl2 = Point(float(x2), float(y2)) #control point center
+        ctrl3 = Point(float(x3), float(y3))
+
+        num_points = 2**iterations + 1
+
+        start_dnc = time.time()
+        dnc_points = bezier_curve(ctrl1, ctrl2, ctrl3, iterations)
+        end_dnc = time.time()
+
+        start_brute = time.time()
+        brute_points = quadratic_bezier(ctrl1, ctrl2, ctrl3, num_points)
+        end_brute = time.time()
+
+        print("Waktu eksekusi Divide and Conquer:", end_dnc-start_dnc , "ms")
+        print("Waktu eksekusi Brute Force:", end_brute-start_brute, "ms")
+
+        plot_bezier_curve(dnc_points)
+        plot_curve(brute_points)
+        print("=======================================")
+        choice = int(input("Choice: "))
+        print("=======================================")
+    if(choice == 2):
+        stop_program = True
+        print("=======================================")
+        print("              Thank You!               ")
+        print("=======================================")
+        print("               13522062                ")
+        print("               13522063                ")
+        print("=======================================")
